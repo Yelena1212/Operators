0c808b4c48672f9409beffd392e5c58ebf3bf499
Yelena1212 <Baranchuk1212@gmail.com>
2020-07-08 22:44:21 -0700

    initial commit

---
+++ EvenOrOdd.java
@@ -0,0 +1,25 @@
+package homework3;
+
+public class EvenOrOdd 
+{
+	public boolean isEven(int number)
+	{
+		return true; //if even
+	}
+
+public static void main(String[] args) 
+{
+    int number = 24;
+	
+	    EvenOrOdd n = new EvenOrOdd();
+	if (number % 2 == 0)
+		System.out.println("The number is even");
+	else 
+		System.out.println("The number is odd"); 
+	
+}
+}jghgfdfbnm,.,mjhrew234567890-p['/§1234567890-=
+
+
+][poiuytrewq	asdfghjkl;'\
+?.,mnbvcxz`
---
+++ src/Operators.java
@@ -0,0 +1,20 @@
+
+public class Operators {
+
+		public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int i = 5;
+		int j = 2;
+		double quotent = ((double)i)/j; // casting
+		System.out.println(quotent);
+		
+		
+		int l = i % j;
+		System.out.println(l); 
+		
+		int x = 3001;
+		System.out.println(x % 8 + 1);
+	}
+
+}
